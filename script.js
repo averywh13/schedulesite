bf39accf8177067aba8af894d371073339d66ba0
averywh13 <awekslerhart@gmail.com>
2024-10-28 14:39:44 -0400

    CHAPTER 1 - got base code for html and JS

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    const btn = $("#submitDay");
+
+    const bellSchedule = {
+        1: { start: '8:24 AM', end: '9:31 AM' },
+        2: { start: '9:36 AM', end: '10:43 AM' },
+        3: { start: '10:48 AM', end: '11:55 AM' },
+        4: { start: '12:41 PM', end: '1:48 PM' },
+        5: { start: '1:53 PM', end: '3:00 PM' }
+      }
+
+
+    btn.on('click', function () {
+        const selectedDay = $("#dayInput").val().trim().toUpperCase();
+        if (!["A", "B", "C", "D", "E", "F", "G"].includes(selectedDay)) {
+            alert("Please enter a letter day!");
+            return;
+        }
+        $.ajax({
+            url: `https://api.npoint.io/1cc58de2a41bbdff2ff9`,
+            method: 'GET',
+            success: function (data) {
+                const schedule = data.schedule
+                const daySchedule = schedule.filter(item => 
+                    item.days.includes(selectedDay),
+                    renderHtml(data)
+                )
+            },
+        });
+    })
+    function renderHtml(data){
+
+    }
+})
